f2c168be1b1c00fced91672d5798381aeec2a19b
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-01-07 17:19:11 +0900

    240107 Java

---
+++ Java/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {  // 주석
+    public static void main(String[] args) {
+        // 한 줄 주석
+        System.out.println("hello java1");  // hello java1 출력
+    //  System.out.println("hello java2");
+
+        /* 여러 줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
---
+++ Java/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {  // 파일명과 클래스 이름이 같아야 한다.
+
+    public static void main(String[] args) {
+        // 출력
+        System.out.println("hello java");  // Java는 문장이 끝날때마다 세미콜론(;)이 필수
+    }
+}
